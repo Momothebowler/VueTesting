cd61f103dc3d754ee3658ca1f732d7c4895334c2
Momothebowler <momothebowler@gmail.com>
2020-08-13 19:38:22 -0400

    Formatting

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    errors:[],
+    message: 'hi',
+    email: null,
+    Password:''
+  },
+  methods:{
+    checkForm: function (e) {
+      this.errors = [];
+
+      if (!this.email) {
+        this.errors.push('Email required.');
+        this.message = "Email required.";
+      } else if (!this.validEmail(this.email)) {
+        this.errors.push('Valid email required.');
+        this.message = "Valid email required.";
+      }
+
+      if (!this.errors.length) {
+        return true;
+      }
+
+      e.preventDefault();
+    },
+    validEmail: function (email) {
+      var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    }
+  }
+})
+
+
+
+
+
 /*var app = new Vue({
     el: '#app',
     data: {
@@ -43,38 +80,7 @@ var app5 = new Vue({
   })
 */
 
-var app6 = new Vue({
-    el: '#app-6',
-    data: {
-      errors:[],
-      message: 'hi',
-      email: null,
-      Password:''
-    },
-    methods:{
-      checkForm: function (e) {
-        this.errors = [];
-  
-        if (!this.email) {
-          this.errors.push('Email required.');
-          this.message = "Email required.";
-        } else if (!this.validEmail(this.email)) {
-          this.errors.push('Valid email required.');
-          this.message = "Valid email required.";
-        }
-  
-        if (!this.errors.length) {
-          return true;
-        }
-  
-        e.preventDefault();
-      },
-      validEmail: function (email) {
-        var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
-      }
-    }
-  })
+
 /*
 
 // Define a new component called todo-item
